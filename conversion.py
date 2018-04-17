7dff8cbc718ee566bbb86d0ec31a505c00fde15e
Kishore Uppada <KishoreUppadaForYou@gmail.com>
2018-04-17 11:19:44 -0700

    Added python definition for dollars to cents conversion

--- conversion.py
+++ conversion.py
@@ -1,17 +1,5 @@
-Testing
+#My Conversion Tools
+def dollars2cents(dollars);
+    cents = dollars * 100
+    return cents
 
-Hello Kishore,
-
-Did you finish your source to target mapping today?
-
-Thanks,
-
-Shi
-
-
-Reply--
-
-Just did but it's on your repository..
-
-Regards,
-Kishroe
